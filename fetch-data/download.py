03fa762f50d7d5dfb5a059874b88cd1b24657569
WildfootW <wild.foot.yee.tzwu@gmail.com>
2018-09-05 22:01:29 +0800

    add fetch-data/download.py

---
+++ fetch-data/download.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+#   Version 
+#   Author: WildfootW
+#   GitHub: github.com/Wildfoot
+#   Copyright (C) 2018 WildfootW All rights reserved.
+#
+
+from ftplib import FTP
+import re
+import os
+
+FTP_SERVER_DOMAIN = "ftp.ntu.edu.tw"
+FTP_SERVER_DIRECTORY = "NTU/course/"
+SAVE_FILE_LOCATION = "./file/"
+
+if __name__ == "__main__":
+
+    if not os.path.exists(SAVE_FILE_LOCATION):
+        os.makedirs(SAVE_FILE_LOCATION)
+
+    ftp = FTP(FTP_SERVER_DOMAIN)
+    ftp.login()
+    ftp.getwelcome()
+    ftp.cwd(FTP_SERVER_DIRECTORY)
+    file_list = []
+    ftp.retrlines("LIST", file_list.append)
+    for i in range(len(file_list)):
+        try:
+            print(file_list[i])
+            file_name = re.search("COURSE.*(X|x)(L|l)(S|s)", file_list[i]).group(0)
+            print(file_name)
+            ftp.retrbinary("RETR " + file_name, open(SAVE_FILE_LOCATION + file_name, "wb").write)
+        except AttributeError:
+            continue
+    ftp.close()
+
